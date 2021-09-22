fe19e215d10445c10f1b6a2e650fb33ba52b7b92
oharper <oharper@atlassian.com>
2021-09-22 18:53:50 +1000

    Lots of stuff done. its pretty nice

--- src/colour.h
+++ src/colour.h
@@ -2,12 +2,30 @@
 #define COLOUR_H
 
 #include "vec3.h"
+#include "rtweekend.h"
 
 void write_colour(unsigned char* pixel, colour pixel_colour) {
     // Write the translated [0,255] value of each colour component.
     pixel[0] = static_cast<char>(255.999 * pixel_colour.x());
     pixel[1] = static_cast<char>(255.999 * pixel_colour.y());
     pixel[2] = static_cast<char>(255.999 * pixel_colour.z());
+}
+
+void write_colour(unsigned char* pixel, colour pixel_colour, int samples_per_pixel) {
+    auto r = pixel_colour.x();
+    auto g = pixel_colour.y();
+    auto b = pixel_colour.z();
+
+    // Divide the color by the number of samples and gamma-correct for gamma=2.0.
+    auto scale = 1.0 / samples_per_pixel;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
+
+    // Write the translated [0,255] value of each colour component.
+    pixel[0] = static_cast<char>(255.999 * r);
+    pixel[1] = static_cast<char>(255.999 * g);
+    pixel[2] = static_cast<char>(255.999 * b);
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,67 @@
 #include <iostream>
+#include <chrono>
+
 #include "stb_image_write.h"
-#include "vec3.h"
+#include "rtweekend.h"
+
+#include "camera.h"
 #include "colour.h"
-#include "ray.h"
+#include "hittable_list.h"
+#include "sphere.h"
 
 using namespace std;
 
-// Image
-const auto aspect_ratio = 16.0 / 9.0;
-const int image_width = 400;
-const int image_height = static_cast<int>(image_width / aspect_ratio);
-const int comps = 3;
+colour ray_colour(const ray& r, const hittable& world, int depth) {
+    hit_record rec;
 
-colour ray_colour(const ray& r) {
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return colour(0,0,0);
+
+    if (world.hit(r, 0.001, infinity, rec)) {
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_colour(ray(rec.p, target - rec.p), world, depth-1);
+    }
     vec3 unit_direction = unit_vector(r.direction());
-    double t = 0.5*(unit_direction.y() + 1.0);
+    double t = 0.5*(unit_direction.y() + 1);
     return (1.0-t)*colour(1.0, 1.0, 1.0) + t*colour(0.5, 0.7, 1.0);
 }
 
 int main() {
+    // Timing
+    auto start = chrono::system_clock::now();
+
     // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int comps = 3;
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
     const string image_name = "image.png";
     unsigned char img_rgb[image_width*image_height*comps];
 
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    world.add(make_shared<sphere>(point3(0,-100.5,0), 100));
+
     // Camera
-    double viewport_height = 2.0;
-    double viewport_width = aspect_ratio * viewport_height;
-    double focal_length = 1.0;
-
-    point3 origin = point3(0, 0, 0);
-    vec3 horizontal = vec3(viewport_width, 0, 0);
-    vec3 vertical = vec3(0, viewport_height, 0);
-    vec3 lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
-
+    camera cam;
 
     // Render
     for (int y = image_height-1; y >= 0; --y) {
         cout << "\rScanlines remaining: " << y << ' ' << std::flush;
         for (int x = 0; x < image_width; ++x) {
-            auto u = double(x) / (image_width-1);
-            auto v = double(y) / (image_height-1);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
-            colour pixel_colour = ray_colour(r);
-
+            colour pixel_colour(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; s++) {
+                auto u = (x + random_double()) / (image_width-1);
+                auto v = (y + random_double()) / (image_height-1);
+                ray r = cam.get_ray(u, v);
+                pixel_colour += ray_colour(r, world, max_depth);
+            }
             unsigned char* pixel_ptr = &img_rgb[(image_height-y-1)*image_width*comps+x*comps];
-            write_colour(pixel_ptr, pixel_colour);
+            write_colour(pixel_ptr, pixel_colour, samples_per_pixel);
         }
     }
     cout << "\nDone scanning.\n";
@@ -59,6 +76,10 @@ int main() {
     } else {
         cout << "Image created at " + image_name << endl;
     }
+
+    auto end = chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    cout << "Seconds elapsed " << elapsed_seconds.count() << endl;
     return 0;
 }
 
